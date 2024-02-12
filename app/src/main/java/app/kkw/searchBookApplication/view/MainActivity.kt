7cce7258bcbf58ca7e0aab14696768d225cd4c9c
KimKyungWon0519 <gwon080870@gmail.com>
2024-02-12 19:24:44 +0900

    FEAT: Connect Adapter

--- app/src/main/java/app/kkw/searchBookApplication/view/MainActivity.kt
+++ app/src/main/java/app/kkw/searchBookApplication/view/MainActivity.kt
@@ -2,11 +2,30 @@ package app.kkw.searchBookApplication.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.LayoutDirection
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import app.kkw.searchBookApplication.R
+import app.kkw.searchBookApplication.model.Book
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initializeRecyclerView();
+    }
+
+    private fun initializeRecyclerView() {
+        val recyclerView: RecyclerView = findViewById(R.id.books_recyclerview)
+        val books: List<Book> = (1..100).map {
+            Book(
+                title = "책 이름 - $it",
+                author = "저자 이름 - $it",
+                publishDate = "$it",
+            )
+        }
+
+        recyclerView.adapter = BookRecyclerAdapter(books)
     }
 }
