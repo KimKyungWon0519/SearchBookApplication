e90274d97159c926a0d49797592651bc612eef3c
KimKyungWon0519 <gwon080870@gmail.com>
2024-02-13 21:41:01 +0900

    FEAT: Create detailed Info Activity

---
+++ app/src/main/java/app/kkw/searchBookApplication/view/detailedInfo/DetailedInfoActivity.kt
@@ -0,0 +1,12 @@
+package app.kkw.searchBookApplication.view.detailedInfo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import app.kkw.searchBookApplication.R
+
+class DetailedInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detailed_info)
+    }
+}
