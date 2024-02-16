66ff2c6162ae6ebdae32acdf15e64b514d079c72
KimKyungWon0519 <gwon080870@gmail.com>
2024-02-16 22:47:38 +0900

    FEAT: Set factory constructor and check isEmpty on Book

--- app/src/main/java/app/kkw/searchBookApplication/model/Book.kt
+++ app/src/main/java/app/kkw/searchBookApplication/model/Book.kt
@@ -3,4 +3,10 @@ package app.kkw.searchBookApplication.model
 data class Book(
     val title: String,
     val author: String,
-)
+) {
+    companion object {
+        fun empty(): Book = Book("", "",)
+    }
+}
+
+fun Book.isEmpty(): Boolean = title.isEmpty() && author.isEmpty()
