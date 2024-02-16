2365ddd87d3ce9baca9678acef1198c57174f3c3
KimKyungWon0519 <gwon080870@gmail.com>
2024-02-16 22:37:54 +0900

    FEAT: Create backbutton on detailed info activity

--- app/src/main/java/app/kkw/searchBookApplication/view/detailedInfo/DetailedInfoActivity.kt
+++ app/src/main/java/app/kkw/searchBookApplication/view/detailedInfo/DetailedInfoActivity.kt
@@ -2,11 +2,29 @@ package app.kkw.searchBookApplication.view.detailedInfo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import app.kkw.searchBookApplication.R
 
 class DetailedInfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detailed_info)
+
+        showNavigationIcon()
+        onClickNavigationIcon()
+    }
+
+    private fun showNavigationIcon() {
+        val toolbar: Toolbar = findViewById(R.id.book_name_toolbar)
+
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back)
+    }
+
+    private fun onClickNavigationIcon() {
+        val toolbar: Toolbar = findViewById(R.id.book_name_toolbar)
+
+        toolbar.setNavigationOnClickListener {
+            finish()
+        }
     }
 }
